e640c787d313ea28b1981ebe9109e7e3021f2d99
Luiz Marroni <104742337+rownaas@users.noreply.github.com>
2024-05-07 10:10:24 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -23,8 +23,8 @@ DEBIAN_FRONTEND=noninteractive apt install -y nginx | tee -a $LOGFILE
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 16
-nvm use 16
+nvm install 18
+nvm use 18
 
 # Configurações regionais e de horário
 timedatectl set-timezone America/Sao_Paulo && \
