7dfc78f33da29a93e3609b3da09fd157d7e5e772
Luiz Marroni <104742337+rownaas@users.noreply.github.com>
2024-05-07 17:11:09 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -28,12 +28,15 @@ DEBIAN_FRONTEND=noninteractive apt update && \
 DEBIAN_FRONTEND=noninteractive apt upgrade -y | tee -a $LOGFILE
 
 # Instalação de dependências de software
-DEBIAN_FRONTEND=noninteractive apt install -y libgbm-dev wget unzip fontconfig locales \
-gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 \
-libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 \
-libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
-libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release \
-xdg-utils python2-minimal build-essential postgresql redis-server libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev | tee -a $LOGFILE
+# DEBIAN_FRONTEND=noninteractive apt install -y libgbm-dev wget unzip fontconfig locales \
+# gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 \
+# libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 \
+# libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
+# libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release \
+# xdg-utils python2-minimal build-essential postgresql redis-server libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev | tee -a $LOGFILE
+
+DEBIAN_FRONTEND=noninteractive apt install -y build-essential postgresql redis-server
+
 
 # Configuração do RabbitMQ
 add-apt-repository -y ppa:rabbitmq/rabbitmq-erlang | tee -a $LOGFILE && \
