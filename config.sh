62f32bc00b0a70a687525632a6ec8f684f273713
Luiz Marroni <104742337+rownaas@users.noreply.github.com>
2024-05-07 10:41:09 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -33,7 +33,7 @@ DEBIAN_FRONTEND=noninteractive apt update && \
 DEBIAN_FRONTEND=noninteractive apt upgrade -y | tee -a $LOGFILE
 
 # Instalação de dependências de software
-DEBIAN_FRONTEND=noninteractive apt install -y npm libgbm-dev wget unzip fontconfig locales \
+DEBIAN_FRONTEND=noninteractive apt install -y libgbm-dev wget unzip fontconfig locales \
 gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 \
 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 \
 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
