dc89d2d42e4e8e05194970cbfe8a7ae17fb3c515
Luiz Marroni <104742337+rownaas@users.noreply.github.com>
2024-05-07 07:56:59 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -29,7 +29,7 @@ gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexp
 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 \
 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release \
-xdg-utils python2-minimal build-essential postgresql redis-server | tee -a $LOGFILE
+xdg-utils python2-minimal build-essential postgresql redis-server nginx=1.21.* | tee -a $LOGFILE
 
 # Configuração do RabbitMQ
 sudo add-apt-repository -y ppa:rabbitmq/rabbitmq-erlang | tee -a $LOGFILE && \
